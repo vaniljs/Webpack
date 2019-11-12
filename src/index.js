2a8704609e33a94fa5c98d7884cd8ce27d33abe1
a.sorokin59@yandex.ru <Mesuti123>
2019-11-13 00:24:30 +0500

    Revert "Сборка Webpack"
    This reverts commit 97394684f9df45cb703a17cdcf95a1883c0f4a34.

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import './style.css';
+import Icon from './img/icon.jpg';
+
+function component() {
+    const element = document.createElement('div');
+    element.classList.add('hello');
+    element.innerHTML = 'Test element';
+
+    const myIcon = new Image();
+    myIcon.src = Icon;
+    element.appendChild(myIcon);
+
+    return element;
+}
+
+
+document.body.appendChild(component());
