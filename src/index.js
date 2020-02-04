52ddea290511f7192189d8771d7eed3b00264d19
a.sorokin59@yandex.ru <Mesuti123>
2020-02-04 14:35:06 +0500

    add import html from index.html

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import "./style.sass";
+import html from './index.html';
 
 ReactDOM.render(<div>Тест</div>, document.getElementById('app'));
