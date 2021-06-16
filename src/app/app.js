096a4d28cbf9ce7a18715af7d9f2ad5006d7bb31
Vaniljs <a.sorokin59@yandex.ru>
2021-06-16 12:59:08 +0500

    add ant design libraries

--- src/app/app.js
+++ src/app/app.js
@@ -1,5 +1,14 @@
 import React from 'react';
 
+import "antd/dist/antd.css";
+import { SmileOutlined } from '@ant-design/icons';
+import { DatePicker } from 'antd';
+
+// Русификация
+import { ConfigProvider } from 'antd';
+import ruRU from 'antd/lib/locale/ru_RU';
+
+
 function importAll(r) {
     let images = {};
 
@@ -11,14 +20,18 @@ function importAll(r) {
 
 const images = importAll(require.context('../images/', true, /\.(png|jpe?g|svg)$/));
 
+
 export function App() {
-    return ( 
-        <div>
-            <img src='./images/test/third.png' /> 
-            <img src='./images/react-light.jpg' /> 
-            <img src={images['second.png']} /> 
-            <img src={images['first.png']} /> 
-            <h1>React JS</h1>
-        </div>
+    return (
+        <ConfigProvider locale={ruRU}>
+            <div>
+                <img src='./images/test/third.png' />
+                <img src='./images/react-light.jpg' />
+                <img src={images['second.png']} />
+                <img src={images['first.png']} />
+                <h1>React JS <SmileOutlined spin/></h1>
+                <DatePicker/>
+            </div>
+        </ConfigProvider>
     )
 }
