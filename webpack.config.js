b89155d9bc6a5eabf42af15265f4585c54ea09f0
Vaniljs <a.sorokin59@yandex.ru>
2021-06-16 12:59:08 +0500

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
         path: path.join(__dirname, '/dist')
     },
     devServer: {
-        contentBase: [path.resolve(__dirname, "dist"), path.resolve(__dirname, "dist")],
+        contentBase: path.resolve(__dirname, "dist"),
         watchContentBase: true,
         open: true,
     },
